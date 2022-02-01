06fb5fefe6071b34d28a15c65eb4e8e3f8835173
Rikard <rikard.nordstrom96@gmail.com>
2022-02-01 15:28:58 +0100

    Bulletuppdatering

--- Assets/Scripts/PlayerBulletController.cs
+++ Assets/Scripts/PlayerBulletController.cs
@@ -8,19 +8,24 @@ public class PlayerBulletController : MonoBehaviour
     public float speed = 20f;
     public Rigidbody2D rb;
 
+    Vector2 lastVelocity;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
+
+        //Döda bullet efter 3 sekunder
+        Invoke("Death", 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lastVelocity = rb.velocity;
     }
 
-
+    //Händelser när bullet träffar specifika colliders
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
@@ -29,10 +34,32 @@ public class PlayerBulletController : MonoBehaviour
             Debug.Log("Enemy");
         }
 
-        if (collision.gameObject.tag == "Wall")
-        {
-            Object.Destroy(gameObject);
-            Debug.Log("Wall");
-        }
+        //if (collision.gameObject.tag == "Wall")
+        //{
+        //    rb.velocity = rb.velocity * -1;
+        //    //Object.Destroy(gameObject);
+        //    Debug.Log("Wall");
+        //}
+
+        //if (collision.gameObject.tag == "Ground")
+        //{
+        //    rb.velocity = rb.velocity * -1;
+        //}
+    }
+
+    //Studsar bullet när den kolliderar med colliders
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector2.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
+
+        Debug.Log("Hit");
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
     }
 }
