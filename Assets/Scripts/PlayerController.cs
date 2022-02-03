5fabb15e2278a5197f7e85c4ef53057448747329
Rikard <rikard.nordstrom96@gmail.com>
2022-02-03 15:40:50 +0100

    Walljump prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,14 +11,15 @@ public class PlayerController : MonoBehaviour
     float xSpeed;
     public float jumpForce;
     bool isGrounded;
-    bool doubleJump;
+    bool wallClimb;
+    int doubleJump;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
-        doubleJump = false;
+        doubleJump = 0;
     }
 
     // Update is called once per frame
@@ -26,14 +27,14 @@ public class PlayerController : MonoBehaviour
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         xSpeed = 10;
-        //Debug.Log(doubleJump);
+        Debug.Log(wallClimb);
 
         //Player jumping
         if (isGrounded == true)
         {
             //Alternativ lösning i ifsatsen efter getbuttondown: Mathf.Abs(playerRigidbody.velocity.y) < 0.001)
 
-            if (Input.GetButtonDown("Jump") && doubleJump == false)
+            if (Input.GetButtonDown("Jump") && doubleJump < 1)
             {
                 //Animation = Jumping
                 animation.SetBool("IsJumping", true);
@@ -43,11 +44,18 @@ public class PlayerController : MonoBehaviour
         }
         else if (isGrounded == false)
         {
-            if (Input.GetButtonDown("Jump") && doubleJump == true)
+            if (Input.GetButtonDown("Jump") && doubleJump >= 1 && wallClimb == false)
             {
                 playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
                 playerRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-                doubleJump = false;
+                doubleJump --;
+            }
+
+            else if (Input.GetButtonDown("Jump") && doubleJump >= 1 && wallClimb == true)
+            {
+                playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
+                playerRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                doubleJump--;
             }
         }
     }
@@ -90,7 +98,17 @@ public class PlayerController : MonoBehaviour
             isGrounded = true;
             //När man är "Grounded" så slutar hoppanimationen att spelas
             animation.SetBool("IsJumping", false);
-            doubleJump = false;
+            doubleJump = 0;
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
+            wallClimb = true;
+            if (doubleJump < 1)
+                { 
+                    doubleJump++;
+                }
         }
     }
 
@@ -100,7 +118,12 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             isGrounded = false;
-            doubleJump = true;
+            doubleJump ++;
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            wallClimb = false;
         }
     }
 }
