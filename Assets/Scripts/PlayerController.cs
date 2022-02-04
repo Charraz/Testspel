e54828c03bbe25fc8e038b98e9acde70f3fa175d
Rikard <rikard.nordstrom96@gmail.com>
2022-02-04 15:16:37 +0100

    Budgetwalljump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,12 @@ public class PlayerController : MonoBehaviour
     float moveHorizontal;
     public float moveSpeed;
 
+    int test;
     //Jumpvariabler
     bool isGrounded;
     public Transform groundCheck;
-    public float checkRadius;
+    public float checkRadiusGround;
+    public float checkRadiusFront;
     public LayerMask whatIsGround;
     public float jumpForce;
     int doubleJump;
@@ -27,7 +29,10 @@ public class PlayerController : MonoBehaviour
     public Transform frontCheck;
     public float wallSlidingSpeed;
     bool wallSliding;
-    bool wallClimb;
+    bool wallJumping;
+    public float xWallForce;
+    public float yWallForce;
+    public float wallJumpTime;
 
     // Start is called before the first frame update
     void Start()
@@ -41,9 +46,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         moveHorizontal = Input.GetAxisRaw("Horizontal");
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-        isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadius, whatIsGround);
-        Debug.Log(isGrounded);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadiusGround, whatIsGround);
+        isTouchingFront = Physics2D.OverlapCircle(frontCheck.position, checkRadiusFront, whatIsGround);
+        Debug.Log(playerRigidbody.velocity);
 
         //Player jumping
         if (isGrounded == true)
@@ -60,44 +65,74 @@ public class PlayerController : MonoBehaviour
         }
         else if (isGrounded == false)
         {
-            if (Input.GetButtonDown("Jump") && doubleJump >= 1 && wallClimb == false)
+            if (Input.GetButtonDown("Jump") && doubleJump >= 1)
             {
                 playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
                 playerRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
                 doubleJump --;
             }
 
-            else if (Input.GetButtonDown("Jump") && doubleJump >= 1 && wallClimb == true)
+            else if (Input.GetButtonDown("Jump") && doubleJump >= 1)
             {
                 playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
                 playerRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
                 doubleJump--;
             }
         }
+
+        if (isTouchingFront == true && isGrounded == false)
+        {
+            wallSliding = true;
+        }
+
+        else
+        {
+            wallSliding = false;
+        }
+
+        if (wallSliding == true)
+        {
+            //playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, Mathf.Clamp(playerRigidbody.velocity.y, -wallSlidingSpeed, float.MaxValue));
+            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, -wallSlidingSpeed);
+        }
+
+        if (Input.GetButtonDown("Jump") && wallSliding == true)
+        {
+            wallJumping = true;
+            Invoke("SetWallJumpToFalse", wallJumpTime);
+        }
+
+        if (wallJumping == true)
+        {
+            playerRigidbody.velocity = new Vector2(xWallForce * -moveHorizontal, yWallForce);
+        }
     }
 
 
     private void FixedUpdate()
     {
-        if (moveHorizontal > 0.1f)
+        if (moveHorizontal > 0.1f && wallJumping == false)
         {
             //Animation = Running
             animation.SetFloat("Speed", moveSpeed);
             //Player movement
             playerRigidbody.velocity = new Vector2(moveSpeed, playerRigidbody.velocity.y);
-            playerSprite.flipX = false;
+            //playerSprite.flipX = false;
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
 
-        else if (moveHorizontal < -0.1f)
+        else if (moveHorizontal < -0.1f && wallJumping == false)
         {
             //Animation = Running
             animation.SetFloat("Speed", moveSpeed);
             //Player movement
             playerRigidbody.velocity = new Vector2(moveSpeed * -1, playerRigidbody.velocity.y);
-            playerSprite.flipX = true;
+            //playerSprite.flipX = true;
+            transform.eulerAngles = new Vector3(0, 180, 0);
+
         }
 
-        else if (moveHorizontal == 0)
+        else if (moveHorizontal == 0 && wallJumping == false)
         {
             //Animation = Idle
             animation.SetFloat("Speed", 0);
@@ -117,15 +152,14 @@ public class PlayerController : MonoBehaviour
             doubleJump = 0;
         }
 
-        if (collision.gameObject.tag == "Wall")
-        {
-            playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
-            wallClimb = true;
-            if (doubleJump < 1)
-                { 
-                    doubleJump++;
-                }
-        }
+        //if (collision.gameObject.tag == "Wall")
+        //{
+        //    playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
+        //    if (doubleJump < 1)
+        //        { 
+        //            doubleJump++;
+        //        }
+        //}
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -136,10 +170,10 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
             doubleJump ++;
         }
+    }
 
-        if (collision.gameObject.tag == "Wall")
-        {
-            wallClimb = false;
-        }
+    void SetWallJumpToFalse()
+    {
+        wallJumping = false;
     }
 }
