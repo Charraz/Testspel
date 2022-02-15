94702d674e5d5b09dc29f2886acdd48e57a567bf
hellooomoerta <60177102+hellooomoerta@users.noreply.github.com>
2022-02-15 15:49:51 +0100

    Rhino Script Rework

---
+++ Assets/Scripts/RhinoBehaviour.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhinoBehaviour : MonoBehaviour
+{
+    private NPCMode npcMode = NPCMode.RhinoWalk;
+    public Rigidbody2D rigidkropp;
+    public new Animator animation;
+
+    float moveSpeed;
+    bool playerInSight;
+    bool movingLeft;
+    //bool seesPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidkropp = gameObject.GetComponent<Rigidbody2D>();
+        animation = gameObject.GetComponent<Animator>();
+
+        movingLeft = true;
+        playerInSight = false;
+        moveSpeed = -3;
+        //seesPlayer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(npcMode);
+        switch (npcMode)
+        {
+            case NPCMode.RhinoWalk:
+                rhinoWalk();
+               
+                //Tittar om spelaren är nära nog för att gå in i sitt attack state
+                RaycastHit2D SeePlayer = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.left), 5f);
+                if (SeePlayer.collider != null && SeePlayer.collider.tag == "Player")
+                {
+                    
+                    npcMode = NPCMode.RhinoRun;
+                    animation.SetBool("SeesPlayer", true);
+                }
+                break;
+
+            case NPCMode.RhinoRun:
+                rhinoRun();
+
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.left) * 0.8f, Color.green);
+                RaycastHit2D HittingSomething = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.left), 0.8f);
+                if (HittingSomething.collider != null && HittingSomething.collider.tag == "Wall")
+                {
+                    npcMode = NPCMode.RhinoWallOrPlayerHit;
+                    animation.SetBool("HitWall", true);
+                }
+
+                break;
+
+            case NPCMode.RhinoWallOrPlayerHit:
+                rhinoWallOrPlayerHit();
+                break;
+
+            //case NPCMode.RhinoHit:
+            //    rhinoHit();
+            //    break;
+
+            default:
+                break;
+        }
+    }
+
+    private void rhinoWalk()
+    {
+        //Sätter bara hastigheten på Rhino i det state där den används
+        moveSpeed = -3;
+
+        //Vänd när Rhino kommer till en vägg
+        RaycastHit2D TurnAroundRaycast = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.left), 1f);
+        if (TurnAroundRaycast.collider != null && TurnAroundRaycast.collider.tag == "Wall")
+        {
+            rigidkropp.transform.Rotate(0f, 180f, 0f);
+            movingLeft = !movingLeft;
+        }
+
+        //Får Rhino att röra sig
+        if (movingLeft == true)
+        {
+            rigidkropp.velocity = new Vector2(moveSpeed, rigidkropp.velocity.y);
+        }
+        else if (movingLeft == false)
+        {
+            rigidkropp.velocity = new Vector2(moveSpeed * -1, rigidkropp.velocity.y);
+        }
+    }
+
+    private void rhinoRun()
+    {
+        moveSpeed = -7;
+
+        //Får Rhino att röra sig
+        if (movingLeft == true)
+        {
+            rigidkropp.velocity = new Vector2(moveSpeed, rigidkropp.velocity.y);
+        }
+        else if (movingLeft == false)
+        {
+            rigidkropp.velocity = new Vector2(moveSpeed * -1, rigidkropp.velocity.y);
+        }
+    }
+
+    private void rhinoWallOrPlayerHit()
+    {
+        animation.SetBool("WallHit", true);
+    }
+
+    public enum NPCMode
+    {
+        RhinoWalk,
+        RhinoRun,
+        RhinoWallOrPlayerHit,
+        RhinoHit
+    }
+}
