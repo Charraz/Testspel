415beaddd9d407b72a056874394b82da7f154817
Rikard <rikard.nordstrom96@gmail.com>
2022-02-22 12:08:14 +0100

    Rocketfunktioner

--- Assets/Scripts/PlayerRocketController.cs
+++ Assets/Scripts/PlayerRocketController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerRocketController : MonoBehaviour
 {
     public float speed;
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
     public GameObject particleExplosion;
+    Collider2D activeExplosion;
+    public float explosionRadius;
+    int hit;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +17,7 @@ public class PlayerRocketController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = transform.right * speed;
 
-        Invoke("Death", 5f);
+        Invoke("Death", 3f);
     }
 
     // Update is called once per frame
@@ -25,6 +28,16 @@ public class PlayerRocketController : MonoBehaviour
 
     void Death()
     {
+        particleExplosion = Instantiate(particleExplosion, transform.position, Quaternion.identity);
+        activeExplosion = Physics2D.OverlapCircle(transform.position, explosionRadius);
+        hit++;
+        Debug.Log(hit);
+
+        if (activeExplosion.tag == "Enemy")
+        {
+            Debug.Log("OOOOO");
+        }
+
         Destroy(gameObject);
     }
 
@@ -32,8 +45,18 @@ public class PlayerRocketController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Enemy")
         {
+
+            hit++;
+            Debug.Log(hit);
             particleExplosion = Instantiate(particleExplosion, transform.position, Quaternion.identity);
-            Destroy(gameObject);
+            activeExplosion = Physics2D.OverlapCircle(transform.position, explosionRadius);
+
+            if (activeExplosion.tag == "Enemy")
+            {
+                Debug.Log("OOOOO");
+            }
         }
+
+        Destroy(gameObject);
     }
 }
