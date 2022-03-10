e08dfe8bcab8520c53376e807ff91d52c6246b91
Rikard <rikard.nordstrom96@gmail.com>
2022-03-10 16:26:30 +0100

    IsGrounded rework

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,7 +9,8 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D playerRigidbody;
     private SpriteRenderer playerSprite;
-
+    private BoxCollider2D playerCollider;
+    [SerializeField] private LayerMask groundMask;
     public new Animator animation;
 
     //Movementvariabler
@@ -20,13 +21,14 @@ public class PlayerController : MonoBehaviour
     //Jumpvariabler
     bool isGrounded;
     public float jumpForce;
-    int doubleJump;
+    bool hasJumped;
     bool canJump;
     bool canDoubleJump;
 
     //Partikelvariabler
     public GameObject dustEffect;
-    bool coroutineAllowed;
+    private bool coroutineAllowed;
+    private bool particleCD;
 
     //PlayerHealth
     public float playerHealth;
@@ -44,11 +46,13 @@ public class PlayerController : MonoBehaviour
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
-        doubleJump = 0;
+        playerCollider = GetComponent<BoxCollider2D>();
+        hasJumped = false;
         canJump = false;
         canDoubleJump = false;
         playerHealth = 5;
         iFrame = false;
+        particleCD = false;
     }
 
     // Update is called once per frame
@@ -58,11 +62,14 @@ public class PlayerController : MonoBehaviour
         //Debug.Log(playerRigidbody.velocity);
 
         //Player jumping
-        if (isGrounded == true)
+        if (IsGrounded())
         {
             //Alternativ lösning i ifsatsen efter getbuttondown: Mathf.Abs(playerRigidbody.velocity.y) < 0.001)
+            hasJumped = false;
+            animation.SetBool("IsJumping", false);
+            coroutineAllowed = true;
 
-            if (Input.GetButtonDown("Jump") && doubleJump < 1)
+            if (Input.GetButtonDown("Jump"))
             {
                 //Animation = Jumping
                 animation.SetBool("IsJumping", true);
@@ -70,15 +77,16 @@ public class PlayerController : MonoBehaviour
                 canJump = true;
                 //Player jumping
 
+                Debug.Log("WOOO");
+
             }
         }
-        else if (isGrounded == false)
+        else if (!IsGrounded())
         {
-            if (Input.GetButtonDown("Jump") && doubleJump >= 1)
+            coroutineAllowed = false;
+            if (Input.GetButtonDown("Jump")  && hasJumped == false)
             {
                 canDoubleJump = true;
-                doubleJump--;
-
             }
         }
 
@@ -149,25 +157,26 @@ public class PlayerController : MonoBehaviour
             canJump = false;
         }
 
-        if (canDoubleJump == true)
+        if (canDoubleJump == true && hasJumped == false)
         {
             playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
             playerRigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             canDoubleJump = false;
+            hasJumped = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Ändrar så att Grounded är true så man kan hoppa igen
-        if (collision.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-            //När man är "Grounded" så slutar hoppanimationen att spelas
-            animation.SetBool("IsJumping", false);
-            doubleJump = 0;
-            coroutineAllowed = true;
-        }
+        //if (collision.gameObject.tag == "Ground")
+        //{
+        //    isGrounded = true;
+        //    //När man är "Grounded" så slutar hoppanimationen att spelas
+        //    animation.SetBool("IsJumping", false);
+        //    doubleJump = 0;
+        //    coroutineAllowed = true;
+        //}
 
         if (collision.gameObject.tag == "Enemy")
         {
@@ -190,32 +199,18 @@ public class PlayerController : MonoBehaviour
                 Invoke("iFrameCD", 0.7f);
             }
         }
-
-        //if (collision.gameObject.tag == "Wall")
-        //{
-        //    playerRigidbody.velocity = new Vector2(playerRigidbody.velocity.x, 0f);
-        //    if (doubleJump < 1)
-        //        { 
-        //            doubleJump++;
-        //        }
-        //}
-
-        //if (collision.gameObject.tag == "Enemy")
-        //{
-        //    playerHealth = playerHealth - 1;
-        //}
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        //Ändrar så att Grounded är false när spelaren lämnar marken
-        if (collision.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-            doubleJump++;
-            coroutineAllowed = false;
-        }
-    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    //Ändrar så att Grounded är false när spelaren lämnar marken
+    //    if (collision.gameObject.tag == "Ground" && IsGrounded())
+    //    {
+    //        isGrounded = false;
+    //        doubleJump++;
+    //        
+    //    }
+    //}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -232,15 +227,33 @@ public class PlayerController : MonoBehaviour
     }
     IEnumerator SpawnDust()
     {
-        while(isGrounded)
+        while(IsGrounded() && particleCD == false)
         {
             Instantiate(dustEffect, new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), dustEffect.transform.rotation);
+            particleCD = true;
+            Invoke("ParticleCD", 0.25f);
             yield return new WaitForSeconds(0.25f);
         }
     }
 
+    private bool IsGrounded()
+    {
+        float extraLength = 0.1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, Vector2.down, extraLength, groundMask);
+
+        //Debug.DrawRay(playerCollider.bounds.center + new Vector3(playerCollider.bounds.extents.x, 0), Vector2.down * (playerCollider.bounds.extents.y + extraLength), Color.green);
+        //Debug.DrawRay(playerCollider.bounds.center - new Vector3(playerCollider.bounds.extents.x, 0), Vector2.down * (playerCollider.bounds.extents.y + extraLength), Color.green);
+        //Debug.DrawRay(playerCollider.bounds.center - new Vector3(0, playerCollider.bounds.extents.y), Vector2.right * (playerCollider.bounds.extents.x), Color.green);
+
+        return raycastHit.collider != null;
+    }
     private void iFrameCD()
     {
         iFrame = false;
+    }
+
+    private void ParticleCD()
+    {
+        particleCD = false;
     }
 }
