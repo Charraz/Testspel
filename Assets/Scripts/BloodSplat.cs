8b4bf468d4ec9b1ff3dfd742eea0e8cbf4a2a8dc
hellooomoerta <60177102+hellooomoerta@users.noreply.github.com>
2022-03-10 16:22:50 +0100

    Blod, start

---
+++ Assets/Scripts/BloodSplat.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSplat : MonoBehaviour
+{
+    public enum SplatLocation
+    {
+        Foreground,
+        Background,
+    }
+
+    public Color backgroundTint;
+    public float minSizeMod = 0.8f;
+    public float maxSizeMod = 1.5f;
+
+    public Sprite[] sprites;
+
+    private SplatLocation splatLocation;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Initialize(SplatLocation splatLocation)
+    {
+        this.splatLocation = splatLocation;
+        SetSprite();
+        SetSize();
+        SetRotation();
+
+        SetLocationProperties();
+    }
+
+    private void SetSprite()
+    {
+        int randomIndex = Random.Range(0, sprites.Length);
+        spriteRenderer.sprite = sprites[randomIndex];
+    }
+
+
+    private void SetSize()
+    {
+        float sizeMod = Random.Range(minSizeMod, maxSizeMod);
+        transform.localScale *= sizeMod;
+    }
+
+
+    private void SetRotation()
+    {
+        float randomRotation = Random.Range(-360f, 360f);
+        transform.rotation = Quaternion.Euler(0f, 0f, randomRotation);
+    }
+
+    private void SetLocationProperties()
+    {
+        switch (splatLocation)
+        {
+            case SplatLocation.Background:
+                spriteRenderer.color = backgroundTint;
+                spriteRenderer.sortingOrder = -100;
+                break;
+
+            case SplatLocation.Foreground:
+                spriteRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+                spriteRenderer.sortingOrder = 6;
+                break;
+        }
+    }
+}
