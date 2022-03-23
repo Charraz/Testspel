fc8fff2a8910ef898a89486bbe98ef14b10a39fe
Rikard <rikard.nordstrom96@gmail.com>
2022-03-23 12:20:07 +0100

    FIXAT

---
+++ Assets/Scripts/UI and Background/PointTextController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointTextController : MonoBehaviour
+{
+    private GameController game;
+    public Text pointText;
+    public int myPoints = 0;
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = GameController.InstanceOfGame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test();
+    }
+
+    private void test()
+    {
+        myPoints = game.points;
+        pointText.text = myPoints.ToString();
+    }
+}
